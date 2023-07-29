f220f6cc0494d51f38cc4da8a8ebfa9fabeb2b68
Krammer4 <bukatovgleb566@gmail.com>
2023-07-30 01:32:13 +0300

    Сделал примерную карточку пользователя и добавил метод сортировки по дате регистрации

--- frontend/src/components/UserCard/UserCard.jsx
+++ frontend/src/components/UserCard/UserCard.jsx
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import "./UserCard.css";
 
-export const UserCard = ({ name }) => {
-  return <div className="user-card">{name}</div>;
+import anonymus from "../../../../frontend/src/img/Profile/none-avatar.png";
+
+export const UserCard = ({ name, avatar, lastName, username }) => {
+  useEffect(() => {
+    console.log(avatar);
+  });
+
+  return (
+    <div className="user-card">
+      <div className="user-card-content">
+        <div className="user-card-row">
+          <div className="user-card-group">
+            {avatar ? (
+              <img
+                className="user-card-avatar"
+                src={`http://localhost:5000/${avatar}`}
+                alt="avatar"
+              />
+            ) : (
+              <img className="user-card-avatar" src={anonymus} alt="avatar" />
+            )}
+            <div>
+              <p className="user-card-name">
+                {name} {lastName}
+              </p>
+              <p className="user-card-username">@{username}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
