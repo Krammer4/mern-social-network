9954744cb463c2856e1bea409b7292ce9ed14198
Krammer4 <bukatovgleb566@gmail.com>
2023-08-21 00:33:01 +0300

    Добавил кружочек с числом запросов в друзья с ReduxToolkit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,38 @@
+import { useDispatch } from "react-redux";
 import "./App.css";
 import { Header } from "./components/Header/Header";
 import { AuthContext } from "./context/authContext";
 import { useAuth } from "./hooks/authHook";
+import { FriendsContext } from "./pages/FriendsPage/FriendsPage";
 import { useRoutes } from "./routes";
+import { useHttp } from "./hooks/httpHook";
+import { useEffect } from "react";
+import { addRequest } from "./redux/slices/requestsSlice";
+import { backend_url } from "./consts";
 
 function App() {
   const { token, login, logout, userId } = useAuth();
   const isAuthentificated = !!token;
   const routes = useRoutes(isAuthentificated);
+
+  const { request, loading, error } = useHttp();
+
+  const userStorageData = JSON.parse(localStorage.getItem("userData"));
+
+  const dispatch = useDispatch();
+
+  const fetchUserData = async () => {
+    const userData = await request(
+      `${backend_url}/api/user/${userStorageData.userId}`,
+      "GET"
+    );
+    dispatch(addRequest(userData.requests));
+  };
+
+  useEffect(() => {
+    fetchUserData();
+  }, []);
+
   return (
     <AuthContext.Provider
       value={{
